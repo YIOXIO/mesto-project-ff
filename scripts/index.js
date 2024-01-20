8c59cec66ef7452e210660e30b5823bf4f2487e5
YIO_XIO <gekkoivan@gmail.com>
2024-01-20 12:26:04 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,12 @@ const cardElements = document.querySelector('.places__list');
 // Создаем новый элемент карточки и заполнеем данными из объекта
 function createCard(data) {
     const card = cardTemplate.cloneNode(true);
-    card.querySelector('.card__image').src = data.link;
-    card.querySelector('.card__image').alt = data.name;
-    card.querySelector('.card__title').textContent = data.name;
-    card.querySelector('.card__delete-button').addEventListener('click', handleDeleteCard)
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => handleDeleteCard(card));    
     return card;
 }
 //Отрисовываем элемент 
@@ -21,6 +23,6 @@ initialCards.forEach((data) => {
 });
 
 // Удалить карточку 
-function handleDeleteCard(evt) {
-    evt.target.closest('.card').remove()
+function handleDeleteCard(card) {
+    card.remove()
 };
