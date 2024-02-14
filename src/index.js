ee1c46126759441dad96b321ecedc67acdad0ba1
unknown <C:\The Bat!>
2024-02-14 14:01:29 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css';
+import {initialCards} from './scripts/cards.js'
+
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 const cardElements = document.querySelector('.places__list');
 
